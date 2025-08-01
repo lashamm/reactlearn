515d129f653fb575b078877ae567adc2653bc1d9
lasha <maisuradze.llasha@gmail.com>
2025-08-01 21:23:14 +0400

    done today

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,17 @@ import styles from "./page.module.css";
 import { Menu } from "@/components/Menu";
 import { MenuItem } from "@/components/MenuItem";
 import Link from "next/link";
-import Button from "./Button/button";
 import TextInput from "./TextInput/textInput";
 import CheckBox from "./CheckBox/CheckBox";
+import Todo from "./Todo/todo";
 
 export default function Home() {
   return (
     <>
-    <Button disabled={false} mode="fill"   />
+    {/* <Button disabled={false} mode="fill"   />
     <TextInput />
-    <CheckBox />
+    <CheckBox /> */}
+    <Todo />
     </>
   );
 }
